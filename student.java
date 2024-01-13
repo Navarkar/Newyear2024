0e40fdffa5b8a03d63e2c70f64d96e87c93027e7
Navarkar <101930890+Navarkar@users.noreply.github.com>
2024-01-13 22:49:33 +0530

    Add files via upload

---
+++ student.java
@@ -0,0 +1,25 @@
+package Inheritance;
+
+public class student extends teacher
+{
+	private static int marks;
+	private static int rno;
+	private static String nm;
+	
+	public student(int marks,int rno,String nm) 
+	{
+		this.marks=marks;
+		this.rno=rno;
+		this.nm=nm;
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args)
+	{
+		student s1=new student(90, 10, "Priya");
+		System.out.println(marks+ " " +rno+ " " +nm+ " " +name+ " " +subject);
+		// TODO Auto-generated method stub
+
+	}
+
+}
